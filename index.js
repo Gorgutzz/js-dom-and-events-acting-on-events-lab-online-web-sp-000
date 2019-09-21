6a26aa6adcb99bf242b438a50a026e651a9cf566
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-09-21 23:14:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,16 @@ function retrieveEmployeeInformation() {
   return document.querySelector("input").value;
 }
 
+function addNewElementAsLi() {
+  const employeeInformation = retrieveEmployeeInformation();
+  const li = document.createElement("li");                        // Create a <p> node
+  const ul = document.querySelector(".employee-list");
+  li.appendChild(document.createTextNode(employeeInformation));
+  ul.appendChild(li);
+  document.querySelector("input").value = "";
+} 
+
+
+
+
 preventRefreshOnSubmit()
