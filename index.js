7aea15d653764541c784979f70a49fcb94a85c97
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-09-22 00:03:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ function addNewElementAsLi() {
 
 function addNewLiOnClick(){
   let submitButton = document.querySelector('input[type="submit"]');
-
   submitButton.addEventListener('click', function(event){
     addNewElementAsLi();
     document.querySelector('input').value = "";
