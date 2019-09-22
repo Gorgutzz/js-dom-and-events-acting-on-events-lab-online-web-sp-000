7d809a705f506cfa743d2e2dbd93d04f114e3970
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-09-22 00:04:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function addNewElementAsLi() {
 }
 
 function addNewLiOnClick(){
-  let submit = document.querySelector('input[type="submit"]');
+  const submit = document.querySelector('input[type="submit"]');
   submit.addEventListener('click', function(event){
     addNewElementAsLi();
     document.querySelector('input').value = "";
